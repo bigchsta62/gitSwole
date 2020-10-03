c60668a7f7fa4906c69b817668b6c89a5197f4a0
bigchsta62 <65703232+bigchsta62@users.noreply.github.com>
2020-10-03 15:56:56 -0700

    background video

--- public/index.js
+++ public/index.js
@@ -11,3 +11,15 @@ async function init() {
   }
 }
 
+var video = document.getElementById("myVideo");
+
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
